5e03df0fd467c73e7a62010d1be1d596e6c96dac
klementtan <klementtan@gmail.com>
2020-01-29 20:40:37 +0800

    Update until how it works

--- src/pages/MiaoMiao/MiaoMiao.js
+++ src/pages/MiaoMiao/MiaoMiao.js
@@ -1,6 +1,7 @@
 // React Basic and Bootstrap
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import howMiaoMiaoWorks from '../../images/MiaoMiao/how_miaomiao_works.png';
 import { Row, Col } from 'reactstrap';
 import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
@@ -100,9 +101,97 @@ class MiaoMiao extends Component {
 
                                 <p className="text-muted">In reference to the Spiderman Movie: "With great power, comes great responsibility"</p>
                                 
-
                                 <p className="text-muted">You are given a powerful tool, the CGM, and it is your responsibility to do finger pricks and try to get the right mindset for it to work.</p>
+                                
+                                <h3 className="mt-4 pt-2">4. CGM vs FGM</h3>
 
+                                <h5 className="mt-4 pt-2">CGM: Continuous Glucose Monitoring</h5>
+                                
+                                <p className="text-muted">
+                                  Why <span className="text-primary font-weight-bold">so chim</span> (complex)... What is CGM? To put it simply, CGM
+                                  <ul className="list-group mt-4">
+                                    <li className="list-inline-item text-muted"> - is a method to track glucose levels throughout the <span className="text-primary font-weight-bold">day and night</span></li>
+                                    <li className="list-inline-item text-muted"> - takes glucose measurements at <span className="text-primary font-weight-bold">regular intervals, 24 hours a day.</span></li>
+                                    <li className="list-inline-item text-muted"> - provides you glucose levels in <span className="text-primary font-weight-bold">real time</span> to help you make more <span className="text-primary font-weight-bold">informed decisions</span> throughout the day</li>
+                                    <li className="list-inline-item text-muted"> - allows you to set high and low BG  <span className="text-primary font-weight-bold">alerts</span> </li>
+                                  </ul>
+                                </p>
+                                <p className="text-muted">
+                                  <ul className="list-group mt-4">
+                                    However, we have to highlight that
+                                    <li className="list-inline-item text-muted"> - CGM<span className="text-primary font-weight-bold"> does not replace</span> finger prick checks for BG</li>
+                                    <li className="list-inline-item text-muted"> - data from CGM is not 100% accurate, hence you must still do regular finger prick checks</li>
+                                  </ul>
+                                </p>
+                                <h5 className="mt-4 pt-2">FGM: Flash Glucose Monitoring</h5>
+
+                                <p className="text-muted">
+                                  <ul className="list-group mt-4">
+                                  Abbott's Freestyle Libre uses the FGM system, which
+                                    <li className="list-inline-item text-muted"> - shows <span className="text-primary font-weight-bold">current</span> glucose reading</li>
+                                    <li className="list-inline-item text-muted"> - takes glucose measurements only when you<span className="text-primary font-weight-bold"> flash the reader</span>  to its sensor</li>
+                                  </ul>
+                                </p>
+
+                                <h5 className="mt-4 pt-2">Still Confused?</h5>
+                                <p className="text-muted">Take a look at this comparison table</p>
+                                {/*Add table*/}
+
+                                <h5 className="mt-4 pt-2">Here are two scenarios to better illustrate the difference</h5>
+                                <p className="text-muted">In both scenarios, Amy goes for a 45 minute bicycle ride in the park. She starts off nicely at 8mmol/L before the ride.</p>
+
+                                <h7 className="mt-4 pt-2">Scenario A:</h7>
+                                <p className="text-muted">30 minutes later, she glances at her phone and sees her BG is at 4mmol/L and receives an alert that BG is dropping fast. She then decides to stop her exercise and take glucose tabs.</p>
+
+                                <h7 className="mt-4 pt-2">Scenario B:</h7>
+                                <p className="text-muted">She is happily enjoying her bicycle ride and forgets to take out her reader/phone to flash at her Libre sensor. After reaching home, she flashed her reader/phone only to realise that she has been low during the last 10 minutes of her exercise.</p>
+                                
+                                <p className="text-muted">In <span className="text-primary font-weight-bold">Scenario A</span> where Amy is using a <span className="text-primary font-weight-bold">CGM</span>, she is able to take necessary actions quickly to react to the fluctuations of BG <span className="text-primary font-weight-bold">while exercising</span>.</p>
+
+                                <p className="text-muted">In <span className="text-primary font-weight-bold">Scenario B</span> where Amy is using a <span className="text-primary font-weight-bold">FGM</span>, she is unaware of her BG while exercising, and received BG data only <span className="text-primary font-weight-bold">after exercising</span>.</p>
+
+                                <h5 className="mt-4 pt-2">TL;DR</h5>
+
+                                <p className="text-muted">CGM offers real time 24 hour BG readings that is more advantageous than the FGM.</p>
+                                
+                                <h3 className="mt-4 pt-2">5. Share BG with Loved Ones</h3>
+
+                                <p className="text-muted">To thrive as a person living with Type 1 Diabetes, we require a good solid support system. By using the CGM, your loved ones can help keep track of your BG too!</p>
+                                <p>
+                                  <ul className="list-group mt-4">
+                                    <h5 className="mt-4 pt-2">Benefits</h5>
+                                    <li className="list-inline-item text-muted"> - Your loved ones will get a peace of mind when you are out alone, or travelling overseas without them</li>
+                                    <li className="list-inline-item text-muted"> - You can keep yourself accountable to your target BG range by getting loved ones involved in helping to remind and motivate you.</li>
+                                    <li className="list-inline-item text-muted"> - Your loved ones can receive alerts for lows or highs anytime of the day</li>
+                                    <li className="list-inline-item text-muted"> - When you are asleep (or are not very good at waking up to alarms), your loved ones can be alerted</li>
+                                  </ul>
+                                </p>
+                                <p>
+                                  <ul className="list-group mt-4">
+                                    <h5 className="mt-4 pt-2">Advice</h5>
+                                    <li className="list-inline-item text-muted"> - Be open to your loved ones in terms of what you would like them to help you with when managing your BG</li>
+                                    <li className="list-inline-item text-muted"> - Be clear on what actions you want your loved ones to take at certain levels of BG(E.g. When I am at 5mmol/L could you remind me to eat a pack of nuts?)</li>
+                                  </ul>
+                                </p>
+                                <h3 className="mt-4 pt-2">6. Life-saving Alerts</h3>
+
+                                <p className="text-muted">No exaggerations, CGM alerts are truly life-saving! Momentarily when we get caught up with life, we forget about taking care of our diabetes. Alerts are most useful during sleep, when you are too occupied at work, traveling, etc. </p>
+                                  <ul className="list-group mt-4">
+                                    <p className="text-muted">The xDrip+ App that is used in tandem with MiaoMiao sounds off if:</p>
+                                    <li className="list-inline-item text-muted"> - BG is high/low </li>
+                                    <li className="list-inline-item text-muted"> - BG is rising/dropping fast</li>
+                                    <li className="list-inline-item text-muted"> - low is predicted in X minutes</li>
+                                  </ul>
+                                <h2>How it works?</h2>
+                                <img class="img-fluid mx-auto d-block" src={howMiaoMiaoWorks} alt="How Miao Miao works flow chart"></img>
+                                <h2>Things you need</h2>
+                                <ul className="list-group mt-4">
+                                  <li className="list-inline-item text-muted"> - Abbott Freestyle Libre Sensor - preferably newly installed</li>
+                                  <li className="list-inline-item text-muted"> - MiaoMiao - we will provide that together with the charger, stickers and armband</li>
+                                  <li className="list-inline-item text-muted"> - Smart Phone to install app for capturing, displaying and storing BG data</li>
+                                  <li className="list-inline-item text-muted"> - Android phones have more support</li>
+                                  <li className="list-inline-item text-muted"> - Apple iPhone app is also available but there may be some issues</li>
+                                </ul>
                                 {/* <p className="text-muted">Copy-paste the style <code className="text-danger font-weight-bold">&lt;link&gt;</code> into your <code className="text-danger font-weight-bold">&lt;head&gt;</code> before all other stylesheets to load Bootstrap CSS.</p> */}
                             </Col>
                         </Row>
